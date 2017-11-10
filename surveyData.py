0f32e597eac2f76b939d0292411590e40cccd56e
Andrew Holt <15341747+ajh1143@users.noreply.github.com>
2017-11-09 16:06:20 -0800

    Create surveyData.py

---
+++ surveyData.py
@@ -0,0 +1,30 @@
+#AJH
+#Sample code for blog:
+#Yields basic information about data repository 
+#prior to machine learning analysis
+
+
+import pandas as pd
+
+def surveyData(data_url, columns)
+   #Access data, apply titles
+   data = pd.read_csv(data_url, names=columns)
+   #find rows and columns
+   print(data.shape)
+   #check data structure
+   print(data.head(3))
+   #find missing values
+   print(data.isnull().sum())
+   #categorize
+   print(data.groupby('Sex').size())
+   #statistics
+   print(data.describe())
+
+#set target
+data_url = "https://archive.ics.uci.edu/ml/
+           machine-learning-databases/abalone/abalone.data"
+#set titles                                                          
+columns = ['Sex','Length','Diameter','Height','WholeWeight', 
+           'ShuckedWeight', 'VisceraWeight', 'ShellWeight', 'Rings']
+#pass target and titles to surveyData and run the program                                     
+def surveyData(data_url, columns)
